3aaa7c313699cfd87ce1e65980beae40cdfa1a85
[ronymathew281] <suyashmore789@gmail.com>
2021-08-27 03:43:49 -0400

    fixing 0 in child process

--- Assignments/Assignment2/2B/Assignment2Bparent.c
+++ Assignments/Assignment2/2B/Assignment2Bparent.c
@@ -1,55 +1,62 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 
 void swap(int *number1, int *number2)
 {
-	int temp = *number1;
-	*number1 = *number2;
-	*number2 = temp;
+    int temp = *number1;
+    *number1 = *number2;
+    *number2 = temp;
 }
 
 void selectionSort(int arr[], int n)
 {
-	int i, j, min_idx;
+    int i, j, min_idx;
 
-	for (i = 0; i < n-1; i++)
-	{
-		min_idx = i;
-		for (j = i+1; j < n; j++)
-		if (arr[j] < arr[min_idx])
-			min_idx = j;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
 
-		swap(&arr[min_idx], &arr[i]);
-	}
+        swap(&arr[min_idx], &arr[i]);
+    }
 }
 
-void sort_array(int argument_count, char* argument_array[], int *integer_array){
-    for(int i=0;i<argument_count;i++){
+void sort_array(int argument_count, char *argument_array[], int *integer_array)
+{
+    for (int i = 0; i < argument_count; i++)
+    {
         *(integer_array + i) = atoi(argument_array[i]);
     }
     selectionSort(integer_array, argument_count);
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     int *integer_array;
-    char **character_array; 
-    if(argc<=1){
+    char **character_array;
+    if (argc <= 1)
+    {
         printf("\nArguments were not provided so no Sorting Will take place..!!\n\n");
     }
-    else{
-    integer_array = (int*)malloc(argc * sizeof(int));
-    character_array = malloc(argc * sizeof(char*));
-    for (int i = 0; i < argc; i++){
-        character_array[i] = malloc(10 * sizeof(char));
-    }
+    else
+    {
+        integer_array = (int *)malloc(argc * sizeof(int));
+        character_array = malloc(argc * sizeof(char *));
+        for (int i = 0; i < argc; i++)
+        {
+            character_array[i] = malloc(10 * sizeof(char));
+        }
 
-    sort_array(argc, argv, integer_array);
-    for(int i=0;i<argc;i++){
-        sprintf(*(character_array+i), "%d", *(integer_array + i));
-    }
-    execve("./child", character_array, NULL);
+        sort_array(argc, argv, integer_array);
+        for (int i = 0; i < argc; i++)
+        {
+            sprintf(*(character_array + i), "%d", *(integer_array + i));
+        }
+        execve("./child", character_array, NULL);
     }
 }
