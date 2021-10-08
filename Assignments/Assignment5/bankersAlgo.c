ffbcdff7d0ca22d621cd7759a13cdf2d471c9f1f
suyash-more <suyashmore789@gmail.com>
2021-10-08 12:22:49 +0530

    bankers issue sorted

--- Assignments/Assignment5/bankersAlgo.c
+++ Assignments/Assignment5/bankersAlgo.c
@@ -3,6 +3,7 @@
 
 
 int n=5;
+
 typedef struct {
     int allocation[3];
     int max[3];
@@ -23,6 +24,7 @@ int getFeasibleProcess(process *processes, int cpu_work_units[]){
 int main(){
     int safe_processes=0;
     int cpu_work_units[3];
+    int copy_cpu_work_units[n][3];
     int pro_no=0;
     process processes[n];
     
@@ -62,7 +64,9 @@ int main(){
         pro_no = getFeasibleProcess(processes, cpu_work_units);
         printf("%d", pro_no);
         if(pro_no!=-1){
-            
+            copy_cpu_work_units[pro_no][0] = cpu_work_units[0]
+            copy_cpu_work_units[pro_no][1] = cpu_work_units[1]
+            copy_cpu_work_units[pro_no][2] = cpu_work_units[]
             cpu_work_units[0] += (processes+pro_no-1)->allocation[0];
             cpu_work_units[1] += (processes+pro_no-1)->allocation[1];
             cpu_work_units[2] += (processes+pro_no-1)->allocation[2];
@@ -77,7 +81,7 @@ int main(){
     printf("\nPro_no\t\tAllocation\t\tMaximum\t\tAvailable\n");
     printf("      \t\tA B C\t\t\t A B C\t\t\tA B C\n");
     for(int i=0;i<n;i++){
-        printf("P%d\t\t%d %d %d\t\t\t%d %d %d\t\t%d %d %d\n",(i+1), (processes+i)->allocation[0],(processes+i)->allocation[1],(processes+i)->allocation[2],(processes+i)->max[0],(processes+i)->max[1],(processes+i)->max[2],cpu_work_units[0], cpu_work_units[1], cpu_work_units[2]);
+        printf("P%d\t\t%d %d %d\t\t\t%d %d %d\t\t%d %d %d\n",(i+1), (processes+i)->allocation[0],(processes+i)->allocation[1],(processes+i)->allocation[2],(processes+i)->max[0],(processes+i)->max[1],(processes+i)->max[2],copy_cpu_work_units[0], copy_cpu_work_units[1], copy_cpu_work_units[2]);
     }
 
     
