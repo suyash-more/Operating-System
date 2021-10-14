a945976bbfbe5dea2995dbc697ab62aab0e9f467
suyash-more <suyashmore789@gmail.com>
2021-10-14 10:36:00 +0530

    resource request algo added

--- Test Codes/resourcerequestalgo.c
+++ Test Codes/resourcerequestalgo.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
-int n=5;
+int n=3;
 
 typedef struct {
     int allocation[3];
@@ -11,7 +10,7 @@ typedef struct {
     int over;
 } process;
 
-int getFeasibleProcess(process *processes, int cpu_work_units[]){
+int get_safe_process(process *processes, int cpu_work_units[]){
     int j=-1;
     for(int i=0;i<n;i++){
         if(((processes+i)->over==0) && (cpu_work_units[0] >= ((processes+i)->need[0])) && (cpu_work_units[1] >= ((processes+i)->need[1])) && (cpu_work_units[2] >= ((processes+i)->need[2]))){
@@ -23,12 +22,11 @@ int getFeasibleProcess(process *processes, int cpu_work_units[]){
 }
 
 int main(){
-    int safe_processes=0;
+    int safe_processes=0, rounds=0;
     int cpu_work_units[3]={0};
-    int copy_cpu_work_units[5][3]={0};
     int pro_no=0;
     process processes[n];
-    
+
     for(int i=0;i<n;i++){
         printf("\n\nEnter the Allocation of resources in P%d\n", (i+1));
         printf("\tA : ");
@@ -42,7 +40,7 @@ int main(){
         printf("\tA : ");
         scanf("%d", &((processes+i)->max[0]));
         printf("\tB : ");
-        scanf("%d", &((processes+i)->max[1]));
+        scanf("%d", &((processes+i)->max[1]));  
         printf("\tC : ");
         scanf("%d", &((processes+i)->max[2]));
         
@@ -52,7 +50,7 @@ int main(){
         
         (processes+i)->over=0;
     }
-    
+
     printf("\n\nEnter the Work units Available\n");
     printf("\tA : ");
     scanf("%d", &cpu_work_units[0]);
@@ -60,31 +58,30 @@ int main(){
     scanf("%d", &cpu_work_units[1]);
     printf("\tC : ");
     scanf("%d", &cpu_work_units[2]);
-    
-    while(safe_processes<=n){
-        pro_no = getFeasibleProcess(processes, cpu_work_units);
+
+    printf("\n\nThe safe Sequence is : ");
+
+    while(safe_processes<n){
+        pro_no = get_safe_process(processes, cpu_work_units);
         printf("P%d ->", pro_no);
         if(pro_no!=-1){
             cpu_work_units[0] += (processes+pro_no)->allocation[0];
             cpu_work_units[1] += (processes+pro_no)->allocation[1];
             cpu_work_units[2] += (processes+pro_no)->allocation[2];
-            copy_cpu_work_units[pro_no][0] = cpu_work_units[0];
-            copy_cpu_work_units[pro_no][2] = cpu_work_units[2];
-            copy_cpu_work_units[pro_no][1] = cpu_work_units[1];
             (processes+pro_no)->over = 1;
             safe_processes++;
         }else{
-            printf("The process is deadlock state..!!");
+            int j=0;
+            printf("\nThe processes do not have enough resources..!!");
+            printf("\n\nThe deadlock is occured due to processes - ");
+            while(j<n){
+                if (((processes+j)->over==0)){
+                    printf(" P%d ", (j+1));
+                    j++;
+                }
+            }
             exit(0);
         }
     }
-    printf(" END\n");
-    
-    printf("\nPro_no\t\tAllocation\t\tMaximum\t\tAfter Available\n");
-    printf("      \t\tA B C\t\t\t A B C\t\t  A B C\n");
-    for(int i=0;i<n;i++){
-        printf("P%d\t\t%d %d %d\t\t\t%d %d %d\t\t  %d %d %d\n",(i+1), (processes+i)->allocation[0],(processes+i)->allocation[1],(processes+i)->allocation[2],(processes+i)->max[0],(processes+i)->max[1],(processes+i)->max[2],copy_cpu_work_units[i][0], copy_cpu_work_units[i][1], copy_cpu_work_units[i][2]);
-    }
-
-    
+    printf("END\n");
 }
